be7d751e47d68bc8fd3a1c9e3f7fdcaeb98bc24d
tecs2000 <tecs@cin.ufpe.br>
2019-11-10 17:30:01 -0300

    Funcao da questao 9 feita, faltando apenas fazer o dataframe.

--- Projeto2Estatistica/questoes.R
+++ Projeto2Estatistica/questoes.R
@@ -1,6 +1,6 @@
 # Integrantes:
 # Gustavo Farani de Farias (gff)
-# Thayná TODO
+# Thayná Cavalcante (tecs)
 # Izael Effemberg da Costa (iec)
 
 # Questão 1
@@ -23,8 +23,31 @@ statMode <- function (y) {
 
 statMode(dados$Qnt..de.Albuns.Vendidos)
 
-#Questão 5
+#Questao 3:
+names <- c(Detalhes_de_Albuns_Página1$`Artista`)
+dates <- c(Detalhes_de_Albuns_Página1$`Ano`)
+publish2018 <- c()
+publish2019 <- c()
+contador = 1
 
+for(x in names){
+  if(dates[contador] == 2018) {
+    publish2018 = c(publish2018, x)
+    
+  }else
+    publish2019 = c(publish2019, x)
+  contador = contador + 1
+}
+publish2018 = unique(publish2018, incomparables = FALSE)
+publish2019 = unique(publish2019, incomparables = FALSE)
+resposta = c(match(publish2018, publish2019))
+
+for(i in resposta){
+  if(!is.na(i))
+    print(publish2019[i])
+}
+
+#Questão 5
 getAlbumMaisVendidoEmUmAno <- function(ano) {
   vendas = max(dados[dados$Ano == ano,][[5]])
   album = dados[dados[5] == vendas,]["Album"]
@@ -47,4 +70,58 @@ getAlbuns <- function(ano) {
 
 print(getAlbuns(2018))
 
+#Questao 6:
+names <- c(Detalhes_de_Albuns_Página1$`Artista`)
+year <- c(Detalhes_de_Albuns_Página1$`Ano`)
+data <- list()
+
+unity = 0
+contador = 0
+contador2 = 1
+
+for(i in names){
+  for(j in names){
+    if(j == i)
+      contador = contador + 1
+  }
+  if(contador == 1){
+    unity = unity + 1
+    data[unity] <- paste("Artista:", i, "||", "Ano:", year[contador2], sep = " ", collapse = NULL) #Concatena e insere
+  }
+  contador2 <- contador2 + 1
+  contador <- 0
+}
+data
+
+#Questao 9:
+empresa <- c(Detalhes_de_Albuns$`Empresa`)
+album <- c(Detalhes_de_Albuns$`Album`)
+vendas <- c(Detalhes_de_Albuns$`Qnt. de Albuns Vendidos`)
+check <- c()
+
+contador = 1
+marcador = 0
+maisVendido = 0
+for(x in empresa){                                  #Varre o array de empresas
+  if(x %in% check == FALSE){                        #checa se aquela empresa ja foi propagada
+    for(y in empresa){                              #propaga o elemento corrente de X
+      if(y == x){                                   #Se ele encontrou a empresa q ta propagando
+        if(vendas[contador] > maisVendido){         #Checa se o valor de vendas relacionado eh maior do que o que ta guardado
+          maisVendido = vendas[contador]
+          data = album[contador]
+        }
+      }
+      contador = contador + 1
+    }
+    print("Empresa:")
+    print(x) 
+    print("Album mais vendido:") 
+    print(data)
+    cat("\n")
+    
+    check <- c(check, x)
+    maisVendido = 0
+    contador = 1
+  }
+}
 
